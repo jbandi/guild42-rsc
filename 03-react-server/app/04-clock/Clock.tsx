c849f803b1ac40be843df2c587fa8fcc9bfc5326
Jonas Bandi <dev@jonasbandi.net>
2024-04-16 14:54:22 +0200

    Cleanup

--- 03-react-server/app/04-clock/Clock.tsx
+++ 03-react-server/app/04-clock/Clock.tsx
@@ -1,15 +1,15 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-
 import React from "react";
 
 export default function Clock() {
   //<editor-fold desc="Hiding crazy demo stuff ...">
   const isClient = typeof window === "object";
   const [time, setTime] = isClient
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     ? React.useState(new Date())
     : [new Date(), () => {}];
 
   if (isClient) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     React.useEffect(() => {
       setInterval(() => {
         setTime(new Date());
