df8b1e34cdc31f4258f15e54ce3d7126105d93cf
Jonas Bandi <dev@jonasbandi.net>
2024-04-16 02:58:52 +0200

    Cleanup

--- 03-react-server/next.config.mjs
+++ 03-react-server/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    experimental: {
+        // 2024-04-16: Does not seem to work ...?
+        // https://nextjs.org/docs/app/api-reference/next-config-js/staleTimes
+        staleTimes: {
+            dynamic: 1,
+            static: 1,
+        }
+    },
 };
 
 export default nextConfig;
