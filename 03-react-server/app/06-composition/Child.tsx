8ea61647b79009ab29af25aa7af47e8448f17e10
Jonas Bandi <dev@jonasbandi.net>
2024-04-16 15:08:23 +0200

    Cleanup

--- 03-react-server/app/06-composition/Child.tsx
+++ 03-react-server/app/06-composition/Child.tsx
@@ -3,6 +3,7 @@ import Shared from "./Shared";
 import styles from "./Child.module.css";
 
 type ChildProps = { children?: React.ReactNode };
+
 export default function Child({ children }: ChildProps) {
   console.log("rendering Child");
   return (
